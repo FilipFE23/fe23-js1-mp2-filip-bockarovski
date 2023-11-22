637b756fd540ec1dd8f7270eff4b9b82235e648b
FilipFE23 <filip.bockarovski@gritacademy.se>
2023-11-22 10:43:51 +0100

    Update visibility/css

--- js/script.js
+++ js/script.js
@@ -4,6 +4,7 @@ let aiScore = 0
 let submitBtn = document.getElementById('submit');
 let descTitle = document.querySelectorAll('p')[2];
 let input = document.getElementById('name');
+let h1El = document.querySelector('h1');
 
 let playerDisplay = document.getElementById('yourPoints');
 let aiDisplay = document.getElementById('aiPoints');
@@ -19,6 +20,7 @@ submitBtn.addEventListener('click', ()=>{
     playerDisplay.innerText = `${playerName}s score: ${yourScore}`;
     document.getElementById('name').value = '';
 
+    h1El.style.margin = '1rem';
     descTitle.style.display = 'none';
     submitBtn.style.display = 'none';
     input.style.display = 'none';
@@ -34,15 +36,18 @@ gameDiv.addEventListener('click', (e)=>{
     if(e.target.id == 'rock'){
         if(randomNumber === 0){
             results.innerText = 'You chose rock and the computer chose rock so it is a draw!';
+            results.style.color = 'black';
         }
         else if(randomNumber === 1){
             aiScore++;
             results.innerText = 'You chose rock and the computer chose paper giving it a point!';
+            results.style.color = 'red';
             aiDisplay.innerText = `Computers score: ${aiScore}`;
         }
         else if(randomNumber === 2){
             yourScore++;
             results.innerText = 'You chose rock and the computer chose scissors giving you a point!';
+            results.style.color = 'green';
             playerDisplay.innerText = `${playerName}s score: ${yourScore}`;
         }
     }
@@ -50,14 +55,17 @@ gameDiv.addEventListener('click', (e)=>{
         if(randomNumber === 0){
             yourScore++;
             results.innerText = 'You chose paper and the computer chose rock giving you a point!';
+            results.style.color = 'green';
             playerDisplay.innerText = `${playerName}s score: ${yourScore}`;
         }
         else if(randomNumber === 1){
             results.innerText = 'You chose paper and the computer chose paper so it is a draw!';
+            results.style.color = 'black';
         }
         else if(randomNumber === 2){
             aiScore++;
             results.innerText = 'You chose paper and the computer chose scissors giving it a point!';
+            results.style.color = 'red';
             aiDisplay.innerText = `Computers score: ${aiScore}`;
         }
     }
@@ -65,26 +73,33 @@ gameDiv.addEventListener('click', (e)=>{
         if(randomNumber === 0){
             aiScore++;
             results.innerText = 'You chose scissors and the computer chose rock giving it a point!';
+            results.style.color = 'red';
             aiDisplay.innerText = `Computers score: ${aiScore}`;
         }
         else if(randomNumber === 1){
             yourScore++;
             results.innerText = 'You chose scissors and the computer chose paper giving you a point!';
+            results.style.color = 'green';
             playerDisplay.innerText = `${playerName}s score: ${yourScore}`;
         }
         else if(randomNumber === 2){
             results.innerText = 'You chose scissors and the computer chose scissors so it is a draw!';
+            results.style.color = 'black';
         };
     };
 
     if(yourScore === 3){
         results.innerText = 'You won the game!';
         showEl[0].classList.remove('hidden');
+
+        results.style.color = 'green';
         gameDiv.style.display = 'none';
     }
     else if(aiScore === 3){
         results.innerText = 'You lost the game!';
         showEl[0].classList.remove('hidden');
+
+        results.style.color = 'red';
         gameDiv.style.display = 'none';
     };
 });
